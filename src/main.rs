4e61f304f268c0dc30b23cb94189a77e97566cc8
Andrei Tecsor <56220194+andreitecsor@users.noreply.github.com>
2022-10-22 23:25:19 +0300

    refactor project part 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
+    //DISTANCE BETWEEN TWO AIRPORTS:
     const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
 
     let kcle_latitude_degrees: f64 = 41.4075;
     let kcle_longitude_degrees: f64 = -81.851111;
-
     let kslc_latitude_degrees: f64 = 40.7861;
     let kslc_longitude_degrees: f64 = -111.9822;
 
